7d327b442c80b3523fb9ea77427563a319f50aef
Nirbhay Kumar Singh <nirbhaysingh.iitm@gmail.com>
2026-08-08 01:15:27 +0530

    Fix GitHub Pages base path

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/publication">
+    <BrowserRouter basename="/radar-portfolio">
       <App />
     </BrowserRouter>
   </React.StrictMode>
